16445a145c1e1dded71f9645a3f5e432701bbe22
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-07 23:29:20 +0600

    all my simple widget will be here

---
+++ lib/widgets/navbar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:insult/const.dart';
+
+class BottomBar extends StatefulWidget {
+  BottomBar({this.onp, this.sInd});
+  final Function onp;
+  final int sInd;
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: primaryColor, boxShadow: [
+        BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+      ]),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            gap: 8,
+            activeColor: Colors.white,
+            color: Colors.black,
+            iconSize: 25,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            duration: Duration(milliseconds: 800),
+            tabBackgroundColor: navBgColor,
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+                iconColor: navIconInactive,
+              ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+                iconColor: navIconInactive,
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Settings',
+                iconColor: navIconInactive,
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+                iconColor: navIconInactive,
+              ),
+            ],
+            selectedIndex: widget.sInd,
+            onTabChange: widget.onp,
+          ),
+        ),
+      ),
+    );
+  }
+}
