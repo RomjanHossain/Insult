688e058651b28d3bfaffa206025e905d1eb0e1b6
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-07 23:28:32 +0600

    adding reuseable const values

---
+++ lib/const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//* all my colors panel are here
+const primaryColor = Color(0xFF313B4E);
+const navIconInactive = Color(0xFF69748E);
+const navBgColor = Color(0xFF0e153a);
+
+Shader linearGradient = LinearGradient(
+  colors: <Color>[
+    Color(0xfffdc168),
+    Color(0xfffb8080),
+  ],
+).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
