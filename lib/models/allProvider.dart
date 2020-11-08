b08b8758308ea2ad49a1d5aad2e172a5aaea583e
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-08 11:35:04 +0600

    lol

---
+++ lib/models/allProvider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+
+class Data extends ChangeNotifier {
+  bool oriented = true;
+  void changeOriented(bool newOri) {
+    oriented = newOri;
+    notifyListeners();
+  }
+}
