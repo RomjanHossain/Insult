a22d8f046f4a94f7c479a43700d0621b7c131e3f
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-09 00:13:39 +0600

    added alphabate and *

--- lib/models/allProvider.dart
+++ lib/models/allProvider.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/foundation.dart';
+import 'package:insult/models/readTxt.dart';
 
 class Data extends ChangeNotifier {
   bool oriented = true;
+  var allSlangs = [];
+  List<String> alpha = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
   void changeOriented(bool newOri) {
     oriented = newOri;
+    notifyListeners();
+  }
+
+  void getAllSlang() async {
+    var data = ReadFile();
+    var allData = await data.parseJson();
+    allSlangs.add(allData);
     notifyListeners();
   }
 }
