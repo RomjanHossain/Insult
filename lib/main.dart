2499b7121d5e3b08be49dd44b56fb4bc2ee8fe03
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-08 11:35:57 +0600

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:insult/screens/homepage.dart';
+import 'package:insult/models/allProvider.dart';
+import 'package:insult/screens/homepage/homepage.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: HomePage(),
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: HomePage(),
+      ),
     );
   }
 }
