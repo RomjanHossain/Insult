08cbb5c25fe9ae76101a7baf83e4accd6248dc76
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-12 00:13:18 +0600

    getting api results

--- lib/models/api/getResultfromAPI.dart
+++ lib/models/api/getResultfromAPI.dart
@@ -1,12 +1,32 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-class GetFromApi {
-  String fapi = 'https://insult.mattbas.org/api/insult';
-  String sapi = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
-  void getRandomInsult() async {
-    Response response = await get(fapi);
-    Response response2 = await get(sapi);
-    print(response.body);
-    print(response2.body);
+String fapi = 'https://insult.mattbas.org/api/insult';
+String sapi = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
+
+class Insults {
+  final String insult;
+  Insults({this.insult});
+  factory Insults.fromJson(Map<String, dynamic> json) {
+    return Insults(
+      insult: json['insult'],
+    );
+  }
+}
+
+Future<Insults> fetchAlbum2() async {
+  final response = await http
+      .get('https://evilinsult.com/generate_insult.php?lang=en&type=json');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+
+    return Insults.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 }
