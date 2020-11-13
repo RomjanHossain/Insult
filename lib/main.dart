e4f1eae11b6a6fcc86e5f4b02fa958f4ae4e77a0
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-14 00:00:39 +0600

    adding firebase instsance

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:insult/services/allProvider.dart';
 import 'package:insult/screens/homepage/homepage.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
