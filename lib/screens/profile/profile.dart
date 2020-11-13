5cf8654c540fdad10317025f55d2a03d9c7de582
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-14 00:01:31 +0600

    ...

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,116 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:insult/screens/loginPage/oneTimeSignUp.dart';
 import 'package:insult/widgets/btn.dart';
 import 'package:insult/widgets/profile_wid/customPaint.dart';
 import 'package:insult/widgets/profile_wid/myCard.dart';
-// import '../../models/api/getResultfromAPI.dart';
+import 'package:insult/services/auth.dart';
+
+AuthServices _auth = AuthServices();
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Insults insult = Insults();
-    return Stack(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // crossAxisAlignment: CrossAxisAlignment.center,
-      // alignment: Alignment.center,
-      children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: CircleAvatar(
-            radius: 80,
-            backgroundImage: AssetImage('assets/images/Profile/profile6.jpeg'),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 5,
-              // vertical: 10,
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: CircleAvatar(
+              radius: 80,
+              backgroundImage:
+                  AssetImage('assets/images/Profile/profile6.jpeg'),
             ),
-            child: CustomPaint(
-              painter: RPSCustomPainter(),
-              child: Container(
-                alignment: Alignment.center,
-                height: 430,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height / 4.5,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          child: MyCard(
-                            Icons.favorite,
-                            'Favourite',
-                          ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height / 4.5,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          child: MyCard(
-                            Icons.edit,
-                            'edited',
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height / 4.5,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          child: MyCard(Icons.whatshot, 'whatshot'),
-                        ),
-                        SizedBox(
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 5,
+                // vertical: 10,
+              ),
+              child: CustomPaint(
+                painter: RPSCustomPainter(),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 430,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 18,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(
                             height: MediaQuery.of(context).size.height / 4.5,
                             width: MediaQuery.of(context).size.width / 2.2,
-                            child: MyCard(Icons.whatshot_rounded, 'rounded')),
-                      ],
-                    ),
-                  ],
+                            child: MyCard(
+                              Icons.emoji_objects,
+                              'Insult added (0)',
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 4.5,
+                            width: MediaQuery.of(context).size.width / 2.2,
+                            child: MyCard(
+                              Icons.edit,
+                              'Word added (0)',
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 4.5,
+                            width: MediaQuery.of(context).size.width / 2.2,
+                            child: MyCard(
+                              Icons.whatshot,
+                              'Definition added (0)',
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 4.5,
+                            width: MediaQuery.of(context).size.width / 2.2,
+                            child: MyCard(
+                              Icons.whatshot_rounded,
+                              'example added (0)',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          top: MediaQuery.of(context).size.height / 1.4,
-          left: MediaQuery.of(context).size.width / 2.6,
-          child: MyBtn(() {
-            print('jslkdjf');
-            // String ss = await insult.getData();
-            // print(ss);
-          }, 'Log Out', Colors.blueGrey),
-        ),
-        Positioned(
-          top: 90,
-          right: 50,
-          child: Container(
-            height: 30,
-            decoration: BoxDecoration(
-              color: Colors.blue[600],
-              borderRadius: BorderRadius.circular(30),
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: 10,
-            ),
-            child: Center(
-              child: Text(
-                'skanthunt49',
-                style: TextStyle(
-                  color: Colors.white,
+          Positioned(
+            top: MediaQuery.of(context).size.height / 1.4,
+            left: MediaQuery.of(context).size.width / 2.6,
+            child: MyBtn(() {
+              print('jslkdjf');
+              // String ss = await insult.getData();
+              // print(ss);
+              _auth.signOut();
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LogInPage()));
+            }, 'Log Out', Colors.blueGrey[800]),
+          ),
+          Positioned(
+            top: 90,
+            right: 50,
+            child: Container(
+              height: 30,
+              decoration: BoxDecoration(
+                color: Colors.blue[600],
+                borderRadius: BorderRadius.circular(30),
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Center(
+                child: Text(
+                  'skanthunt49',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
