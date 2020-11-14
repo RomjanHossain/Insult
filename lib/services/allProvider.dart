19b6ffaee9e29a88c47d8f1ec509382f63f16dcf
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-14 23:49:45 +0600

    bool

--- lib/services/allProvider.dart
+++ lib/services/allProvider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class Data extends ChangeNotifier {
   bool oriented = true;
+  bool isUser = false;
 
   void changeOriented(bool newOri) {
     oriented = newOri;
