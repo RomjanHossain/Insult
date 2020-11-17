27d3f43830824c7e6b6d23851798ea39a11463ed
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-17 23:22:38 +0600

    isuse bool del

--- lib/services/allProvider.dart
+++ lib/services/allProvider.dart
@@ -2,7 +2,8 @@ import 'package:flutter/foundation.dart';
 
 class Data extends ChangeNotifier {
   bool oriented = true;
-  bool isUser = false;
+  bool showFloatingActionbtn = true;
+  bool showappbarActions = true;
 
   void changeOriented(bool newOri) {
     oriented = newOri;
