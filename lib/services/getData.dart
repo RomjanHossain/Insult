51a69a093f0cabef505fdce0db7f723d6f9a702e
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-18 00:05:53 +0600

    .

--- lib/services/getData.dart
+++ lib/services/getData.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:insult/services/auth.dart';
 
 class GetData {
+  AuthServices _auth = AuthServices();
   FirebaseFirestore _db = FirebaseFirestore.instance;
 }
