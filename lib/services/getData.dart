bafcfc3cc4330f4b70f010a9323fd252f5ffb2b2
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-18 16:16:25 +0600

    get data from firebase

--- lib/services/getData.dart
+++ lib/services/getData.dart
@@ -1,7 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:insult/services/auth.dart';
 
 class GetData {
-  AuthServices _auth = AuthServices();
   FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  getAllInsult() async {
+    var doc = _db.collection('insults').get();
+    await doc.then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((element) {
+        element.data().forEach((key, value) {
+          print(value[0]);
+        });
+      });
+    });
+  }
 }
+
+/*
+// document snapshot
+FirebaseFirestore.instance
+    .collection('users')
+    .doc(userId)
+    .get()
+    .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        print('Document data: ${documentSnapshot.data()}');
+      } else {
+        print('Document does not exist on the database');
+      }
+    });
+
+// querysnapshot
+FirebaseFirestore.instance
+    .collection('users')
+    .get()
+    .then((QuerySnapshot querySnapshot) => {
+        querySnapshot.docs.forEach((doc) {
+            print(doc["first_name"]);
+        });
+    });
+
+*/
