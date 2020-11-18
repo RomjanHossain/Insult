573bc4649628461a0421a7431a8e54f406ef43cd
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-18 16:17:39 +0600

    post data to firebase

---
+++ lib/services/postData.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class PD {
+  int idNum;
+  String userName;
+  String insult;
+  PD({this.idNum, this.insult, this.userName});
+  List postdata() {
+    return [idNum, userName, insult];
+  }
+}
+
+class PostData {
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  postinAllInsult() async {
+    var doc2 = _db.collection('insults').doc('allInsults').set({
+      'id': 6,
+      'insult': [
+        'romjan14',
+        'do your keepers a huge favor: do a triple summersault through the air, and disappear up your own asshole!'
+            'romjan22',
+        'who the duck'
+      ],
+    });
+    print('comple');
+    return doc2;
+  }
+
+  Future<void> newPostMethod(User user) {
+    DocumentReference reportRef = _db.collection('insults').doc(user.uid);
+    return reportRef.update(
+      {
+        'post88': ['is it working?', 'romj4an62']
+      },
+    );
+  }
+}
