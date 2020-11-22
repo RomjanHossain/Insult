d3ed1097e3af899cee1c3ee4b1b584e47ceaca87
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-22 14:51:01 +0600

    .

--- lib/services/getData.dart
+++ lib/services/getData.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class GetData {
   FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -13,6 +14,29 @@ class GetData {
       });
     });
   }
+
+  getLength(User usr) async {
+    int length;
+    var doc = _db.collection('insults').doc(usr.uid).get();
+    await doc.then((DocumentSnapshot documentSnapshot) {
+      // print(documentSnapshot.data().length);
+      length = documentSnapshot.data().length;
+      // return documentSnapshot.data().length;
+    });
+    return length;
+  }
+
+  //! this from realtime database
+  // readData() async {
+  //   var allWordsFromDatabase = databaseReference.child('Words');
+  //   return allWordsFromDatabase;
+  // databaseReference.once().then(
+  //   (DataSnapshot snapshot) {
+  //     print('Data : ${snapshot.value}');
+  //     return snapshot.value;
+  //   },
+  // );
+  // }
 }
 
 /*
