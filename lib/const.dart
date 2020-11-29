e9fab48924a76077234a9b72ad6ca773a687c24c
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-30 00:13:40 +0600

    adding font

--- lib/const.dart
+++ lib/const.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF313B4E);
 const navIconInactive = Color(0xFF69748E);
 const navBgColor = Color(0xFF0e153a);
+const testColor = Color(0xFF2e2e2e);
 
 Shader linearGradient = LinearGradient(
   colors: <Color>[
@@ -15,39 +16,47 @@ Shader linearGradient = LinearGradient(
 const textSTyle = TextStyle(
   color: Colors.black,
   fontSize: 30,
+  fontFamily: 'Raleway',
 );
 
 const bestQuote = TextStyle(
   color: Colors.white,
   fontSize: 50,
   letterSpacing: .2,
+  fontFamily: 'Raleway',
 );
 
 const wordQ = TextStyle(
   color: Colors.white,
   fontSize: 50,
   letterSpacing: 1.5,
+  fontFamily: 'Raleway',
 );
 
 const nounQ = TextStyle(
   color: Colors.black,
   fontSize: 30,
+  fontFamily: 'Raleway',
 );
 const adjQ = TextStyle(
   color: Colors.black,
+  fontFamily: 'Raleway',
   fontSize: 30,
 );
 
 const exampQ = TextStyle(
   color: Colors.black,
+  fontFamily: 'Raleway',
   fontSize: 30,
 );
 
 const notIndb = TextStyle(
   color: Colors.white,
+  fontFamily: 'Raleway',
   fontSize: 20,
 );
 const loading = TextStyle(
   color: Colors.white,
+  fontFamily: 'Raleway',
   fontSize: 20,
 );
