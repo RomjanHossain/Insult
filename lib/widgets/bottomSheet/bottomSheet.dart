01bcdb78d29ebc822921fe701df79ad7d87e719e
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-30 15:12:25 +0600

    provider added extra comment del and mybtn2

--- lib/widgets/bottomSheet/bottomSheet.dart
+++ lib/widgets/bottomSheet/bottomSheet.dart
@@ -1,22 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:insult/services/auth.dart';
+import 'package:insult/const.dart';
+
 import 'package:insult/services/postData.dart';
 import 'package:insult/widgets/btn.dart';
+import 'package:provider/provider.dart';
 
-AuthServices _auth = AuthServices();
 PostData _post = PostData();
 
 class MyBottomSheet extends StatelessWidget {
   MyBottomSheet({this.ll});
   final int ll;
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    var usr = Provider.of<User>(context);
     String value;
     return Material(
       child: Container(
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: testColor,
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(30),
             topRight: Radius.circular(30),
@@ -30,78 +36,81 @@ class MyBottomSheet extends StatelessWidget {
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.lightBlueAccent,
+                fontFamily: 'Raleway',
                 fontSize: 30,
               ),
             ),
             SizedBox(
               height: 10,
             ),
-            TextFormField(
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-              ),
-              textInputAction: TextInputAction.done,
-              maxLines: 10,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 1,
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                validator: (val) {
+                  if (val.length < 10) {
+                    return 'must have 11 char at least';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (String val) {
+                  value = val;
+                },
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Raleway',
+                  fontSize: 30,
+                ),
+                textInputAction: TextInputAction.done,
+                maxLines: 10,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  hintText: 'Your insult here....',
+                  hintStyle:
+                      TextStyle(color: Colors.white60, fontFamily: 'Raleway'),
+                  focusColor: Colors.black,
+                  // focusedBorder: InputBorder.none,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  labelText: 'Insult',
+                  labelStyle: TextStyle(
+                    color: Colors.white60,
                   ),
                 ),
-                hintText: 'Your insult here....',
-                hintStyle: TextStyle(
-                  color: Colors.black54,
-                ),
-                focusColor: Colors.black,
-                // focusedBorder: InputBorder.none,
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                  ),
-                ),
-                labelText: 'Insult',
-                labelStyle: TextStyle(
-                  color: Colors.black54,
-                ),
+                // autofocus: true,
+                textAlign: TextAlign.center,
+                onChanged: (val) async {
+                  value = val;
+                  //! post data
+                },
               ),
-              // autofocus: true,
-              textAlign: TextAlign.center,
-              onChanged: (val) async {
-                value = val;
-                //! post data
-              },
             ),
             SizedBox(
               height: 10,
             ),
-            MyBtn(
-              () async {
-                // print(value);
-
-                // for (String ins in _fromSite) {
-                //   var usr = await _auth.getUser();
-                //   await _post.newPostMethod(usr, ins, _newLL);
-                //   _newLL += 1;
-                //   print(ins);
-                //   print(_newLL);
-                // }
-                // print(_fromSite.length);
-
-                // _fromSite.forEach((element) {
-                //   print(element);
-                // });
-
-                var usr = await _auth.getUser();
-                await _post.newPostMethod(usr, value, ll);
-                Navigator.pop(context);
-              },
-              'Add',
-              Colors.blue,
+            SizedBox(
+              height: 50,
+              width: 100,
+              child: MyBtn2(() async {
+                if (_formKey.currentState.validate()) {
+//    If all data are correct then save data to out variables
+                  _formKey.currentState.save();
+                  await _post.newPostMethod(usr, value, ll);
+                  Navigator.pop(context);
+                }
+              }, 'Add', Colors.blue, Colors.green),
             )
           ],
         ),
