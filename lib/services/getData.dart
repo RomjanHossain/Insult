141cf663351920899f08d358a6a6964d98024623
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-30 15:12:40 +0600

    comment del

--- lib/services/getData.dart
+++ lib/services/getData.dart
@@ -43,29 +43,3 @@ class GetData {
   // );
   // }
 }
-
-/*
-// document snapshot
-FirebaseFirestore.instance
-    .collection('users')
-    .doc(userId)
-    .get()
-    .then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        print('Document data: ${documentSnapshot.data()}');
-      } else {
-        print('Document does not exist on the database');
-      }
-    });
-
-// querysnapshot
-FirebaseFirestore.instance
-    .collection('users')
-    .get()
-    .then((QuerySnapshot querySnapshot) => {
-        querySnapshot.docs.forEach((doc) {
-            print(doc["first_name"]);
-        });
-    });
-
-*/
