40817111cdb39e0e9ee6161777cf7d2c9e0e02eb
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-02 13:18:59 +0600

    getuserInsult length

--- lib/services/allProvider.dart
+++ lib/services/allProvider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/foundation.dart';
+import 'package:insult/services/auth.dart';
+import 'package:insult/services/getData.dart';
+
+GetData _data = GetData();
+AuthServices _auth = AuthServices();
 
 class Data extends ChangeNotifier {
   bool oriented = true;
   bool showFloatingActionbtn = true;
   bool showappbarActions = true;
+  int userInsultLength = 0;
 
   void changeOriented(bool newOri) {
     oriented = newOri;
+    notifyListeners();
+  }
+
+  void getUserInsultLength() async {
+    var user = await _auth.getUser();
+    userInsultLength = await _data.getLength(user);
     notifyListeners();
   }
 }
